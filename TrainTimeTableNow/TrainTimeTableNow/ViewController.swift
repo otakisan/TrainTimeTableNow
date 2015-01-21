6807d1289ec942488114860a4db7683cd188ff80
otakisan <takashi@aquamarine.local>
2015-01-21 15:49:52 +0900

    プロジェクト新規作成

---
+++ TrainTimeTableNow/TrainTimeTableNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainTimeTableNow
+//
+//  Created by takashi on 2015/01/21.
+//  Copyright (c) 2015年 Takashi Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
